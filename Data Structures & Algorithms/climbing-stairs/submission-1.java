f716010b1b20c179132cc63b4650f30b93ca73d2
Reese Cantu <65140498+reesecantu@users.noreply.github.com>
2026-07-17 10:59:20 -0400

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        // the number of ways to climb n stairs is
+        // climbStairs(n - 1) + climbStairs(n - 2) + 3
+        if (n <= 3) return n;
+        int[] dp = new int[n + 1];
+        
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        } 
+        return dp[n];
+    }
+}
